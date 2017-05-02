013f6e409f3f665bf8a6e8ab8210b82c6b34eeea
PachoAZ <pachoarguello@gmail.com>
2017-05-01 19:16:27 -0500

    COmmit 2

---
+++ WcfService1/IRestServiceImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IRestServiceImpl" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method="GET",
+            ResponseFormat=WebMessageFormat.Json,
+            BodyStyle=WebMessageBodyStyle.Wrapped,
+            UriTemplate="json/{id}")]
+        string DoWork(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "Suma/{res}")]
+        long Suma(string res);
+    }
+}
---
+++ WcfService1/RestServiceImpl.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "RestServiceImpl" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione RestServiceImpl.svc o RestServiceImpl.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        public string DoWork(string id)
+        {
+            return "Hola Mundo hoy es viernes " + id;
+
+        }
+        public long Suma(string res)
+        {
+            int a=1;
+            int b=2;
+            //int c = res.Cast<int>().First();
+            int d = b * 2;
+            return d;
+
+        }
+
+    }
+
+}
